9b8d28ff5db92b414eaf51de7dc414dcbdaaa99b
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-11-01 00:51:25 +0400

    function

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function findTotal(){
+    var arr = document.getElementsByName('course');
+    var result=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            result += parseInt(arr[i].value);
+    }
+    document.getElementById('total').value = result;
+}

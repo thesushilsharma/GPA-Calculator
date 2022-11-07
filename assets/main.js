0f6d51e29043f8d8b8360a5319cfa44043b066b7
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2022-11-08 01:59:19 +0400

    add subject

--- assets/main.js
+++ assets/main.js
@@ -13,3 +13,20 @@ function clearScreen(){
     document.getElementById('total').value = ''
 
 }
+
+function add_subject() {
+    var parent = document.getElementById('wam_calculator');
+    var cnt = 0;
+    for (var i = 0; i < parent.childNodes.length; i++) {
+      if (parent.childNodes[i].className == 'dark:bg-slate-900') {
+        cnt++;
+      }
+    }
+    var newChild =
+    '<tr class="dark:bg-slate-900">'+
+    '<td><label for="subject_' + cnt + '" class="sr-only"></label><input type="text" class="subject" id="subject_' + cnt + '"><span class="validate_subject">&nbsp;</span></td>' +
+    '<td><label for="credit_' + cnt + '" class="sr-only"></label><input type="text" class="credit" id="credit_' + cnt + '"><span class="validate_credit">&nbsp;</span></td>' +
+    '<td><label for="mark_' + cnt + '" class="sr-only"></label><input type="text" class="mark" id="mark_' + cnt + '"><span class="validate_mark">&nbsp;</span></td>' +
+    '</tr>';
+    parent.insertAdjacentHTML('beforeend', newChild);
+  }
